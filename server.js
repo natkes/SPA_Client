fc13aa3383ef51a6940687bcb79e6f8e03777b8d
natkes <itompir5@gmail.com>
2020-12-08 23:34:01 -0800

    Added CORS

--- server.js
+++ server.js
@@ -3,9 +3,13 @@ const app = express();
 
 app.use(express.static("./dist/SpaClient"));
 
-app.get("/*", function(req, res) {
-  res.sendFile("index.html", {root: "./dist/SpaClient"}
-);
+app.get("/*", function(req, res,next) {
+  res.sendFile("index.html", {root: "./dist/SpaClient"});
+  res.header("Access-Control-Allow-Origin", '*');
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+  next();
 });
 
 app.listen(process.env.PORT || 8080);
