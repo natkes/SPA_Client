456e16e15fe5f5ce640bb513ca36ddd894d48e8a
natkes <itompir5@gmail.com>
2020-12-08 23:38:41 -0800

    Update Server.JS

--- server.js
+++ server.js
@@ -7,8 +7,6 @@ app.get("/*", function(req, res,next) {
   res.sendFile("index.html", {root: "./dist/SpaClient"});
   res.header("Access-Control-Allow-Origin", '*');
   res.header("Access-Control-Allow-Credentials", true);
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
   next();
 });
 
