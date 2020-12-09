715bafdb622be3274f21dcc857bc07cb72792c49
natkes <itompir5@gmail.com>
2020-12-08 23:53:25 -0800

    Fixed CORS issue

--- server.js
+++ server.js
@@ -5,9 +5,6 @@ app.use(express.static("./dist/SpaClient"));
 
 app.get("/*", function(req, res,next) {
   res.sendFile("index.html", {root: "./dist/SpaClient"});
-  res.header("Access-Control-Allow-Origin", '*');
-  res.header("Access-Control-Allow-Credentials", true);
-  next();
 });
 
 app.listen(process.env.PORT || 8080);
